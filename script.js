0ef412866aa11526f2d744ffea6b8cebcb908bde
yousifmahayri1 <yousifmahayri1@gmail.com>
2026-08-18 02:15:25 +0300

    didnt understand these

--- script.js
+++ script.js
@@ -496,3 +496,33 @@ fruits.sort().reverse()
 
 // reversed alphabatical 
 
+// spread operator 
+
+// allows an iterable such as an array or string to be expanded into seperate elements 
+
+let numbersArray = [1 , 2 , 3, 4 ,5 ]
+
+let minimum = Math.min(...numbersArray)
+
+// didnt understand this one 
+
+// rest parameters = allow a function work with a variable number of arguments by bundling 
+// them into an array 
+
+// the opposite of spread 
+
+function openFridge (...foods) {
+    console.log(foods)
+}
+
+// this function is designed to accept whatever number of arguments 
+
+const food1 = 'pizza'
+const food2 = 'ham'
+const food3 = 'hotdog'
+const food4 = 'sushi'
+
+openFridge(food1 , food2 , food3 , food4)
+
+//random password generator 
+
